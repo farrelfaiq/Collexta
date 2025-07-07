1c39b8b8c595c812f4dec97490bd2c077edcb6cc
farrelfaiq_ <farrelruliff165@gmail.com>
2025-07-08 01:18:52 +0700

    add pbHooks go

---
+++ pb_hooks/pb_hooks.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+  "github.com/pocketbase/pocketbase"
+  "github.com/pocketbase/pocketbase/core"
+)
+
+func main() {
+  app := pocketbase.New()
+
+  app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+    e.Router.Use(func(next http.Handler) http.Handler {
+      return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Access-Control-Allow-Origin", "https://collexta.my.id")
+        w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, PATCH, DELETE, OPTIONS")
+        w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+        w.Header().Set("Access-Control-Allow-Credentials", "true")
+
+        // Untuk request preflight
+        if r.Method == "OPTIONS" {
+          w.WriteHeader(http.StatusOK)
+          return
+        }
+
+        next.ServeHTTP(w, r)
+      })
+    })
+
+    return nil
+  })
+
+  app.Start()
+}
